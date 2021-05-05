7b0b84cb8757ee88a58ae211587ff38859cb5f4d
Electwix <ege.tuzun@gmail.com>
2021-05-05 20:53:15 +0300

    Update and rename __resource.lua to fxmanifest.lua

--- cutscene/__resource.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'adamant'
-games { 'gta5' };
-
-client_scripts {
-    'custune.lua',
-}
-
-
---
+++ cutscene/fxmanifest.lua
@@ -0,0 +1,13 @@
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+
+author 'Doublox'
+description 'Cutscene'
+version '1.0.0'
+
+
+client_scripts {
+    'custune.lua',
+}
