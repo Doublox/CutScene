df2bc88a37ade9a412f218e5d2fd301e144b22fd
Electwix <ege.tuzun@gmail.com>
2021-05-05 20:51:08 +0300

    Update custune.lua

--- cutscene/custune.lua
+++ cutscene/custune.lua
@@ -1,48 +1,77 @@
-RegisterCommand("start", function() -- CREATION DE LA COMMANDE
-    print("anuma") 
-    local plyrId = PlayerPedId() -- ID DU JOUEURS
-    local playerClone = ClonePed_2(plyrId, 0.0, false, true, 1) ----
-    -----------------------------------------------
-    RequestCutscene("MP_INTRO_CONCAT", 8) ---- REQUEST la custscene
-    print("oui")
-    while not HasCutsceneLoaded() do Wait(10) end --- attente que la cutscene se load
-    print("oui1")
-    DoScreenFadeIn(250) --- ecran blanc de 250 milliseconde
+RegisterCommand("start", function(source, args, rawCommand)
+    TriggerEvent('axe-cinematic:start')
+end, false)
+
+local cutsene = false
+
+RegisterNetEvent('axe-cinematic:start') -- Event The start the Cutscene.
+AddEventHandler("axe-cinematic:start", function()
+    cutsene = true
+
+    local plyrId = PlayerPedId() 
+    local playerClone = ClonePed_2(plyrId, 0.0, false, true, 1) 
+
+    RequestCutscene("MP_INTRO_CONCAT", 8)
+    while not HasCutsceneLoaded() do Wait(10) end 
+
+    DoScreenFadeOut(1000)
+    Wait(2000)
+    DoScreenFadeIn(2000)
+
+
     print("ou3")
     SetCutsceneEntityStreamingFlags('MP_Male_Character', 0, 1) 
-    local female = RegisterEntityForCutscene(0,"MP_Female_Character",3,0,64)                                                          ------------ REMPLACER PAR VOTRE JOUEUR
+    local female = RegisterEntityForCutscene(0,"MP_Female_Character",3,0,64)                                                         
     RegisterEntityForCutscene(PlayerPedId(), 'MP_Male_Character', 0, GetEntityModel(PlayerPedId()), 64)
     GetEntityIndexOfCutsceneEntity('MP_Female_Character', GetHashKey(GetEntityModel('MP_Female_Character')))
     NetworkSetEntityInvisibleToNetwork(female, true)
-    -----------------------------------------------
-    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_1", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
---SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_2", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
-    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_3", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
-    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_4", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
-    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_5", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
-    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_6", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
-    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_7", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_1', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_2', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_3', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_4', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_5', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_6', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_7', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    ---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_1', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-   -- RegisterEntityForCutscene(0, 'MP_Plane_Passenger_2', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_3', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_4', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_5', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_6', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_7', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
-    NewLoadSceneStartSphere(-1212.79, -1673.52, 7, 1000, 0) ----- eviter les bugs de texture
-    -----------------------------------------------
-    SetWeatherTypeNow("EXTRASUNNY") ---- TEMPS SOLEIL
-    StartCutscene(4) --- START la custscene
 
-    -------si vous voulez arrétez à un temps précis -- Vous pouvez vous aidez grace GetCutsceneTime() vous le printer
-    -- Wait(10000) --- temps de la custscene
-   -- StopCutsceneImmediately()
+
+    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_1", 0, 1)
+
+    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_3", 0, 1)
+    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_4", 0, 1)
+    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_5", 0, 1)
+    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_6", 0, 1)
+    SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_7", 0, 1)
+    
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_1', 3, GetHashKey('mp_f_freemode_01'), 0)-
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_2', 3, GetHashKey('mp_f_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_3', 3, GetHashKey('mp_f_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_4', 3, GetHashKey('mp_f_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_5', 3, GetHashKey('mp_f_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_6', 3, GetHashKey('mp_f_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_7', 3, GetHashKey('mp_f_freemode_01'), 0)
+    
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_1', 3, GetHashKey('mp_m_freemode_01'), 0)
+   
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_3', 3, GetHashKey('mp_m_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_4', 3, GetHashKey('mp_m_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_5', 3, GetHashKey('mp_m_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_6', 3, GetHashKey('mp_m_freemode_01'), 0)
+    RegisterEntityForCutscene(0, 'MP_Plane_Passenger_7', 3, GetHashKey('mp_m_freemode_01'), 0)
+    NewLoadSceneStartSphere(-1212.79, -1673.52, 7, 1000, 0)
+
+    StartCutscene(4) 
+
+    Citizen.CreateThread(function()
+
+        while cutsene do
+            Citizen.Wait(100)
+            print(GetCutsceneTime())
+            if GetCutsceneTime() >= 37000 then
+                DoScreenFadeOut(1000)
+                Wait(2000)
+                DoScreenFadeIn(2000)
+
+
+                StopCutsceneImmediately()
+                cutsene = false
+            end
+        end
+
+
+
+
+    end)
 end) 
