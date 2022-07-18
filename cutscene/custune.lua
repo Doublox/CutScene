71d09cba4c0bbccd6925ef8746d989e26c682081
Lior <45363723+HirboSH@users.noreply.github.com>
2022-07-18 22:41:29 +0300

    🛠️ Updated cutscene.lua
    🛠️ Updated cutscene.lua
    
    - Made code simpler, and easier to read.
    - Removed ``IsMale`` function, there is a native function we can use.
    - Added customization support for the passengers.
    
    TODO:
    - For now, its adding random textures to all of the passengers. (Generating random texture id in the range of the options). For a future update, textures will be customizable too.

--- cutscene/custune.lua
+++ cutscene/custune.lua
@@ -1,4 +1,4 @@
-local sub_b0b5 = {
+local pedsList = {
     [0] = "MP_Plane_Passenger_1",
     [1] = "MP_Plane_Passenger_2",
     [2] = "MP_Plane_Passenger_3",
@@ -8,281 +8,106 @@ local sub_b0b5 = {
     [6] = "MP_Plane_Passenger_7"
 }
 
-function sub_b747(ped, a_1)
-    if a_1 == 0 then
-        SetPedComponentVariation(ped, 0, 21, 0, 0)
-        SetPedComponentVariation(ped, 1, 0, 0, 0)
-        SetPedComponentVariation(ped, 2, 9, 0, 0)
-        SetPedComponentVariation(ped, 3, 1, 0, 0)
-        SetPedComponentVariation(ped, 4, 9, 0, 0)
-        SetPedComponentVariation(ped, 5, 0, 0, 0)
-        SetPedComponentVariation(ped, 6, 4, 8, 0)
-        SetPedComponentVariation(ped, 7, 0, 0, 0)
-        SetPedComponentVariation(ped, 8, 15, 0, 0)
-        SetPedComponentVariation(ped, 9, 0, 0, 0)
-        SetPedComponentVariation(ped, 10, 0, 0, 0)
-        SetPedComponentVariation(ped, 11, 10, 0, 0)
-        ClearPedProp(ped, 0)
-        ClearPedProp(ped, 1)
-        ClearPedProp(ped, 2)
-        ClearPedProp(ped, 3)
-        ClearPedProp(ped, 4)
-        ClearPedProp(ped, 5)
-        ClearPedProp(ped, 6)
-        ClearPedProp(ped, 7)
-        ClearPedProp(ped, 8)
-    elseif a_1 == 1 then
-        SetPedComponentVariation(ped, 0, 13, 0, 0)
-        SetPedComponentVariation(ped, 1, 0, 0, 0)
-        SetPedComponentVariation(ped, 2, 5, 4, 0)
-        SetPedComponentVariation(ped, 3, 1, 0, 0)
-        SetPedComponentVariation(ped, 4, 10, 0, 0)
-        SetPedComponentVariation(ped, 5, 0, 0, 0)
-        SetPedComponentVariation(ped, 6, 10, 0, 0)
-        SetPedComponentVariation(ped, 7, 11, 2, 0)
-        SetPedComponentVariation(ped, 8, 13, 6, 0)
-        SetPedComponentVariation(ped, 9, 0, 0, 0)
-        SetPedComponentVariation(ped, 10, 0, 0, 0)
-        SetPedComponentVariation(ped, 11, 10, 0, 0)
-        ClearPedProp(ped, 0)
-        ClearPedProp(ped, 1)
-        ClearPedProp(ped, 2)
-        ClearPedProp(ped, 3)
-        ClearPedProp(ped, 4)
-        ClearPedProp(ped, 5)
-        ClearPedProp(ped, 6)
-        ClearPedProp(ped, 7)
-        ClearPedProp(ped, 8)
-    elseif a_1 == 2 then
-        SetPedComponentVariation(ped, 0, 15, 0, 0)
-        SetPedComponentVariation(ped, 1, 0, 0, 0)
-        SetPedComponentVariation(ped, 2, 1, 4, 0)
-        SetPedComponentVariation(ped, 3, 1, 0, 0)
-        SetPedComponentVariation(ped, 4, 0, 1, 0)
-        SetPedComponentVariation(ped, 5, 0, 0, 0)
-        SetPedComponentVariation(ped, 6, 1, 7, 0)
-        SetPedComponentVariation(ped, 7, 0, 0, 0)
-        SetPedComponentVariation(ped, 8, 2, 9, 0)
-        SetPedComponentVariation(ped, 9, 0, 0, 0)
-        SetPedComponentVariation(ped, 10, 0, 0, 0)
-        SetPedComponentVariation(ped, 11, 6, 0, 0)
-        ClearPedProp(ped, 0)
-        ClearPedProp(ped, 1)
-        ClearPedProp(ped, 2)
-        ClearPedProp(ped, 3)
-        ClearPedProp(ped, 4)
-        ClearPedProp(ped, 5)
-        ClearPedProp(ped, 6)
-        ClearPedProp(ped, 7)
-        ClearPedProp(ped, 8)
-    elseif a_1 == 3 then
-        SetPedComponentVariation(ped, 0, 14, 0, 0)
-        SetPedComponentVariation(ped, 1, 0, 0, 0)
-        SetPedComponentVariation(ped, 2, 5, 3, 0)
-        SetPedComponentVariation(ped, 3, 3, 0, 0)
-        SetPedComponentVariation(ped, 4, 1, 6, 0)
-        SetPedComponentVariation(ped, 5, 0, 0, 0)
-        SetPedComponentVariation(ped, 6, 11, 5, 0)
-        SetPedComponentVariation(ped, 7, 0, 0, 0)
-        SetPedComponentVariation(ped, 8, 2, 0, 0)
-        SetPedComponentVariation(ped, 9, 0, 0, 0)
-        SetPedComponentVariation(ped, 10, 0, 0, 0)
-        SetPedComponentVariation(ped, 11, 3, 12, 0)
-        ClearPedProp(ped, 0)
-        ClearPedProp(ped, 1)
-        ClearPedProp(ped, 2)
-        ClearPedProp(ped, 3)
-        ClearPedProp(ped, 4)
-        ClearPedProp(ped, 5)
-        ClearPedProp(ped, 6)
-        ClearPedProp(ped, 7)
-        ClearPedProp(ped, 8)
-    elseif a_1 == 4 then
-        SetPedComponentVariation(ped, 0, 18, 0, 0)
-        SetPedComponentVariation(ped, 1, 0, 0, 0)
-        SetPedComponentVariation(ped, 2, 15, 3, 0)
-        SetPedComponentVariation(ped, 3, 15, 0, 0)
-        SetPedComponentVariation(ped, 4, 2, 5, 0)
-        SetPedComponentVariation(ped, 5, 0, 0, 0)
-        SetPedComponentVariation(ped, 6, 4, 6, 0)
-        SetPedComponentVariation(ped, 7, 4, 0, 0)
-        SetPedComponentVariation(ped, 8, 3, 0, 0)
-        SetPedComponentVariation(ped, 9, 0, 0, 0)
-        SetPedComponentVariation(ped, 10, 0, 0, 0)
-        SetPedComponentVariation(ped, 11, 4, 0, 0)
-        ClearPedProp(ped, 0)
-        ClearPedProp(ped, 1)
-        ClearPedProp(ped, 2)
-        ClearPedProp(ped, 3)
-        ClearPedProp(ped, 4)
-        ClearPedProp(ped, 5)
-        ClearPedProp(ped, 6)
-        ClearPedProp(ped, 7)
-        ClearPedProp(ped, 8)
-    elseif a_1 == 5 then
-        SetPedComponentVariation(ped, 0, 27, 0, 0)
-        SetPedComponentVariation(ped, 1, 0, 0, 0)
-        SetPedComponentVariation(ped, 2, 7, 3, 0)
-        SetPedComponentVariation(ped, 3, 11, 0, 0)
-        SetPedComponentVariation(ped, 4, 4, 8, 0)
-        SetPedComponentVariation(ped, 5, 0, 0, 0)
-        SetPedComponentVariation(ped, 6, 13, 14, 0)
-        SetPedComponentVariation(ped, 7, 5, 3, 0)
-        SetPedComponentVariation(ped, 8, 3, 0, 0)
-        SetPedComponentVariation(ped, 9, 0, 0, 0)
-        SetPedComponentVariation(ped, 10, 0, 0, 0)
-        SetPedComponentVariation(ped, 11, 2, 7, 0)
-        ClearPedProp(ped, 0)
-        ClearPedProp(ped, 1)
-        ClearPedProp(ped, 2)
-        ClearPedProp(ped, 3)
-        ClearPedProp(ped, 4)
-        ClearPedProp(ped, 5)
-        ClearPedProp(ped, 6)
-        ClearPedProp(ped, 7)
-        ClearPedProp(ped, 8)
-    elseif a_1 == 6 then
-        SetPedComponentVariation(ped, 0, 16, 0, 0)
-        SetPedComponentVariation(ped, 1, 0, 0, 0)
-        SetPedComponentVariation(ped, 2, 15, 1, 0)
-        SetPedComponentVariation(ped, 3, 3, 0, 0)
-        SetPedComponentVariation(ped, 4, 5, 6, 0)
-        SetPedComponentVariation(ped, 5, 0, 0, 0)
-        SetPedComponentVariation(ped, 6, 2, 8, 0)
-        SetPedComponentVariation(ped, 7, 0, 0, 0)
-        SetPedComponentVariation(ped, 8, 2, 0, 0)
-        SetPedComponentVariation(ped, 9, 0, 0, 0)
-        SetPedComponentVariation(ped, 10, 0, 0, 0)
-        SetPedComponentVariation(ped, 11, 3, 7, 0)
-        ClearPedProp(ped, 0)
-        ClearPedProp(ped, 1)
-        ClearPedProp(ped, 2)
-        ClearPedProp(ped, 3)
-        ClearPedProp(ped, 4)
-        ClearPedProp(ped, 5)
-        ClearPedProp(ped, 6)
-        ClearPedProp(ped, 7)
-        ClearPedProp(ped, 8)
+local ComponentsTypes = { 
+    [0] = Config.Faces,
+    [1] = Config.Masks,
+    [2] = Config.Hairs,
+    [3] = Config.Torsos,
+    [4] = Config.Legs,
+    [5] = Config.ParachuteBags,
+    [6] = Config.Shoes,
+    [7] = Config.Accessories,
+    [8] = Config.Undershirts,
+    [9] = Config.Kevlars,
+    [10] = Config.Badges,
+    [11] = Config.Torsos2s
+}
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function()
+	PrepareMusicEvent("FM_INTRO_START")
+	TriggerMusicEvent("FM_INTRO_START")
+
+    local playerId = PlayerPedId()
+    
+	if IsPedMale(playerId) then 
+        RequestCutsceneWithPlaybackList("MP_INTRO_CONCAT", 31, 8)
+    else 
+        RequestCutsceneWithPlaybackList("MP_INTRO_CONCAT", 103, 8) 
     end
-end
 
-RegisterCommand("start", function() -- Command for trigger
-	TriggerEvent('introCinematic:start') -- you can use this for trigger the cutscene
-end) 
+    while not HasCutsceneLoaded() do Wait(10) end
 
-
-
-RegisterNetEvent('introCinematic:start') -- ORDER CREATION
-AddEventHandler('introCinematic:start', function()
-	PrepareMusicEvent("FM_INTRO_START") --FM_INTRO_START
-	TriggerMusicEvent("FM_INTRO_START") --FM_INTRO_START
-    local plyrId = PlayerPedId() -- PLAYER ID
-    -----------------------------------------------
-	if IsMale(plyrId) then
-		RequestCutsceneWithPlaybackList("MP_INTRO_CONCAT", 31, 8)
-	else	
-		RequestCutsceneWithPlaybackList("MP_INTRO_CONCAT", 103, 8)
-	end
-    while not HasCutsceneLoaded() do Wait(10) end --- waiting for the cutscene to load
-	if IsMale(plyrId) then
-		RegisterEntityForCutscene(0, 'MP_Male_Character', 3, GetEntityModel(PlayerPedId()), 0)
-		RegisterEntityForCutscene(PlayerPedId(), 'MP_Male_Character', 0, 0, 0)
+	if IsPedMale(playerId) then
+		RegisterEntityForCutscene(0, 'MP_Male_Character', 3, GetEntityModel(playerId), 0)
+		RegisterEntityForCutscene(playerId, 'MP_Male_Character', 0, 0, 0)
 		SetCutsceneEntityStreamingFlags('MP_Male_Character', 0, 1) 
-		local female = RegisterEntityForCutscene(0,"MP_Female_Character",3,0,64) 
+
+		local female = RegisterEntityForCutscene(0 ,"MP_Female_Character", 3 ,0 ,64) 
 		NetworkSetEntityInvisibleToNetwork(female, true)
 	else
-		RegisterEntityForCutscene(0, 'MP_Female_Character', 3, GetEntityModel(PlayerPedId()), 0)
-		RegisterEntityForCutscene(PlayerPedId(), 'MP_Female_Character', 0, 0, 0)
+		RegisterEntityForCutscene(0, 'MP_Female_Character', 3, GetEntityModel(playerId), 0)
+		RegisterEntityForCutscene(playerId, 'MP_Female_Character', 0, 0, 0)
 		SetCutsceneEntityStreamingFlags('MP_Female_Character', 0, 1) 
-		local male = RegisterEntityForCutscene(0,"MP_Male_Character",3,0,64) 
+        
+		local male = RegisterEntityForCutscene(0, "MP_Male_Character", 3, 0, 64) 
 		NetworkSetEntityInvisibleToNetwork(male, true)
 	end
-	local ped = {}
-	for v_3=0, 6, 1 do
-		if v_3 == 1 or v_3 == 2 or v_3 == 4 or v_3 == 6 then
-			ped[v_3] = CreatePed(26, `mp_f_freemode_01`, -1117.77783203125, -1557.6248779296875, 3.3819, 0.0, 0, 0)
+
+	local peds = {}
+	for pedIdx = 0, 6, 1 do
+		if pedIdx == 1 or pedIdx == 2 or pedIdx == 4 or pedIdx == 6 then
+			peds[pedIdx] = CreatePed(26, `mp_f_freemode_01`, -1117.77783203125, -1557.6248779296875, 3.3819, 0.0, 0, 0)
 		else
-			ped[v_3] = CreatePed(26, `mp_m_freemode_01`, -1117.77783203125, -1557.6248779296875, 3.3819, 0.0, 0, 0)
+			peds[pedIdx] = CreatePed(26, `mp_m_freemode_01`, -1117.77783203125, -1557.6248779296875, 3.3819, 0.0, 0, 0)
 		end
-        if not IsEntityDead(ped[v_3]) then
-			sub_b747(ped[v_3], v_3)
-            FinalizeHeadBlend(ped[v_3])
-            RegisterEntityForCutscene(ped[v_3], sub_b0b5[v_3], 0, 0, 64)
+
+        if not IsEntityDead(peds[pedIdx]) then
+			HandlePassengersClothes(peds[pedIdx], pedIdx)
+            FinalizeHeadBlend(peds[pedIdx])
+            RegisterEntityForCutscene(peds[pedIdx], pedsList[pedIdx], 0, 0, 64)
         end
     end
 	
-	NewLoadSceneStartSphere(-1212.79, -1673.52, 7, 1000, 0) ----- avoid texture bugs
-    -----------------------------------------------
-    SetWeatherTypeNow("EXTRASUNNY") ---- SUN TIME
-    StartCutscene(4) --- START the custscene
+	NewLoadSceneStartSphere(-1212.79, -1673.52, 7, 1000, 0)
 
-    Wait(31520) --- custscene time
-	for v_3=0, 6, 1 do
-		DeleteEntity(ped[v_3])
-	end
+    SetWeatherTypeNow(Config.WeatherType)
+    StartCutscene(4)
+
+    Wait(31520)
+
+	for pedIdx = 0, 6, 1 do DeleteEntity(peds[pedIdx]) end
+
 	PrepareMusicEvent("AC_STOP")
 	TriggerMusicEvent("AC_STOP")
-end) 
+end)
 
-
-function IsMale(ped)
-	if IsPedModel(ped, 'mp_m_freemode_01') then
-		return true
-	else
-		return false
-	end
+function ClearPedProps(ped)
+    for i = 0, 8, 1 do
+        ClearPedProp(ped, i)
+    end
 end
 
--- Thanks to PERPGamer to contribut
+function HandleRandomPeds(ped)
+    SetPedRandomComponentVariation(ped, 0) 
+    ClearPedProps(ped)
+end
 
+function HandlePassengersClothes(ped, pedIdx)
+    if Config.RandomPassengersClothes then
+        if pedIdx >= 0 and pedIdx <= 6 then HandleRandomPeds(ped) end
+    else
+        for i = 0, 6, 1 do
+            if pedIdx >= 0 and pedIdx <= 6 then
+                for j = 0, 11, 1 do
+                    local component = ComponentsTypes[j]
+                    local numberOfTextures = GetNumberOfPedTextureVariations(ped, j, component[i])
+                    local randomTexture = math.random(numberOfTextures)
 
-
--- The default script will present in my video
--- RegisterCommand("start", function() -- CREATION DE LA COMMANDE
---     print("anuma") 
---     local plyrId = PlayerPedId() -- ID DU JOUEURS
---     local playerClone = ClonePed_2(plyrId, 0.0, false, true, 1) ----
---     -----------------------------------------------
---     RequestCutscene("MP_INTRO_CONCAT", 8) ---- REQUEST la custscene
---     print("oui")
---     while not HasCutsceneLoaded() do Wait(10) end --- attente que la cutscene se load
---     print("oui1")
---     DoScreenFadeIn(250) --- ecran blanc de 250 milliseconde
---     print("ou3")
---     SetCutsceneEntityStreamingFlags('MP_Male_Character', 0, 1) 
---     local female = RegisterEntityForCutscene(0,"MP_Female_Character",3,0,64)                                                          ------------ REMPLACER PAR VOTRE JOUEUR
---     RegisterEntityForCutscene(PlayerPedId(), 'MP_Male_Character', 0, GetEntityModel(PlayerPedId()), 64)
---     GetEntityIndexOfCutsceneEntity('MP_Female_Character', GetHashKey(GetEntityModel('MP_Female_Character')))
---     NetworkSetEntityInvisibleToNetwork(female, true)
---     -----------------------------------------------
---     SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_1", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
--- --SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_2", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
---     SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_3", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
---     SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_4", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
---     SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_5", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
---     SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_6", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
---     SetCutsceneEntityStreamingFlags("MP_Plane_Passenger_7", 0, 1)---PARTIE POUR DELETE LES AUTRE PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_1', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_2', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_3', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_4', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_5', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_6', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_7', 3, GetHashKey('mp_f_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     ---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_1', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---    -- RegisterEntityForCutscene(0, 'MP_Plane_Passenger_2', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_3', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_4', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_5', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_6', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     RegisterEntityForCutscene(0, 'MP_Plane_Passenger_7', 3, GetHashKey('mp_m_freemode_01'), 0)---PARTIE POUR DELETE LES PEDS
---     NewLoadSceneStartSphere(-1212.79, -1673.52, 7, 1000, 0) ----- eviter les bugs de texture
---     -----------------------------------------------
---     SetWeatherTypeNow("EXTRASUNNY") ---- TEMPS SOLEIL
---     StartCutscene(4) --- START la custscene
-
---     -------si vous voulez arrétez à un temps précis -- Vous pouvez vous aidez grace GetCutsceneTime() vous le printer
---     -- Wait(10000) --- temps de la custscene
---    -- StopCutsceneImmediately()
--- end) 
+                    SetPedComponentVariation(ped, j, component[i], randomTexture, 0)
+                    ClearPedProps(ped)
+                end
+            end
+        end
+    end
+end
